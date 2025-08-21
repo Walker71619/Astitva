1f727dfb7809b7865656845ad2420ff8bca770fc
Mahira Khan <mahirakhan24538@gmail.com>
2025-08-21 23:10:32 +0530

    updated astitva

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./home.css";
+import Bg1 from "../images/Bg1.jpg";
 
 function Home() {
   return (
-    <div className="home-container">
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${Bg1})` }}
+    >
       <div className="text-section">
         <h1 className="title">ASTITVA</h1>
         <p className="subtitle">Your Life Blueprint</p>
