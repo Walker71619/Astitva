634ca81b6a701266b5d25b47f23f2fcea10e2ac3
Mahira Khan <153006911+Walker71619@users.noreply.github.com>
2025-08-23 14:19:31 +0530

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,8 @@
 import React from "react";
 import "./home.css";
-import Bg1 from "../images/Bg1.jpg";
 
+import BgMain from "../images/Bg_main.png";   
+import Bg2    from "../images/Bg2.png";       
+import Moon   from "../images/Moon.png";
+import Tree   from "../images/Tree.png";
+import Flower from "../images/Flower.png";    // optional: small accent/demo
