3830c007a4c9ecbabedea3bbb2f3ca6f2d443000
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-08-28 00:00:54 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./views/home";
-import KarmicAI from "./views/KarmicAI"; 
+import Home from "./views/HomePage";
+import KarmicAI from "./views/KarmicAI";
 import DharmaScheduler from "./views/DharmaScheduler";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
+
+        {/* Karmic AI */}
         <Route path="/karmic-ai" element={<KarmicAI />} />
+
+        {/* Dharma Scheduler */}
         <Route path="/dharma-scheduler" element={<DharmaScheduler />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
