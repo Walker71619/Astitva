d78bf4ad48ad27e13b379ee9fccb2a809bf68622
Mahira Khan <153006911+Walker71619@users.noreply.github.com>
2025-08-27 20:02:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Home from './views/home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./views/home";
+import KarmicAI from "./views/KarmicAI"; 
+import DharmaScheduler from "./views/DharmaScheduler";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/karmic-ai" element={<KarmicAI />} />
+        <Route path="/dharma-scheduler" element={<DharmaScheduler />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
